21130d47920280fedbfd129e6b1dab14e0466500
Aleksandr <sbfmede21@gmail.com>
2021-10-25 13:37:31 +0300

    find button and submit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,10 +1,14 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 public class MainClass {
+
     public static void main(String [] args) throws InterruptedException {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\otikhanov\\Apps\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
@@ -13,15 +17,30 @@ public class MainClass {
 
         driver.manage().window().maximize();
 
-        driver.get("https://www.google.com/");
-        driver.navigate().to("https://www.selenium.dev/");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+        driver.findElement(By.xpath("//div/input[@id=\"searchButton\"]")).click();
+
+        driver.get("https://github.com/");
+
+        WebElement button = driver.findElement(By.xpath("//div/button[@class=\"btn-mktg width-full width-sm-auto btn-primary-mktg\"]"));
+        if (button.getText().equals("Sign up for GitHub")) {
+            System.out.println("Success!");
+        }
+        else System.out.println("Fail!");
+
+
+        System.out.println();
+
+        button.submit();
+
+
+
+
 
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 
-        driver.quit();
+        //driver.quit();
          }
 }
