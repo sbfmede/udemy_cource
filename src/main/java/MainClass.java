17d7d6cd8fbdc0cdc9f503745d8f88fc5041c6ea
Aleksandr <sbfmede21@gmail.com>
2021-10-26 18:05:18 +0300

    Table methods added, main class chnaged

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,4 @@
+import com.google.common.collect.Table;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,12 +13,10 @@ public class MainClass {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\otikhanov\\Apps\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.manage().window().maximize();
 
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+       /* driver.get("https://en.wikipedia.org/wiki/Main_Page");
 
         driver.findElement(By.xpath("//div/input[@id=\"searchButton\"]")).click();
 
@@ -40,12 +39,17 @@ public class MainClass {
 
         driver.findElement(By.xpath("//div/a[@class=\"_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy\"]")).submit();
 
-
-
-
-
         System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl());*/
+
+        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        WebElement tableElement = driver.findElement(By.xpath("//table[@id='customers']"));
+        Table table = new Table (tableElement, driver);
+
+        System.out.println("Rows number is: " + table.getRows().size());
+
+        System.out.println(table.getValueFromCell(2,3));
+        System.out.println(table.getValueFromCell(4,1));
 
         //driver.quit();
          }
